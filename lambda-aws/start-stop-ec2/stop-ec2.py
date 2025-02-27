f9b6092a218386eccfc46a2657870b0b9ee96331
itsp-technology <vivek1996.cloud@gmail.com>
2025-02-27 14:48:50 +0530

    wp

--- lambda-aws/start-stop-ec2/stop-ec2.py
+++ lambda-aws/start-stop-ec2/stop-ec2.py
@@ -1,6 +1,6 @@
 import boto3  
 region = 'us-east-1'
-nstances = ['i-0648877a98fb941e9', 'i-0f6be46fbe136f281']
+instances = ['i-0648877a98fb941e9', 'i-0f6be46fbe136f281']
 ec2 = boto3.client('ec2', region_name=region)
        
 def lambda_handler(event, context):
